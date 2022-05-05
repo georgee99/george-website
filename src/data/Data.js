1ed0f442d32d7ef6aa2c5f6d6a6067159d7e9aa5
georgee99 <g.elzakhem99@gmail.com>
2022-05-05 19:08:39 +1000

    Added words

--- src/data/Data.js
+++ src/data/Data.js
@@ -10,7 +10,7 @@ export const projects = [
       title: "Geek Flix Movie App",
       subtitle: "Angular",
       description:
-        "An application which fetches movies from TMDB API, and renders them paginated. Users can sort movies by emotion, or delete.",
+        "An application which fetches movies from TMDB API, and renders them paginated. Users can sort movies by emotion, or remove them from the collection.",
       image: project1Image,
       link: "https://github.com/georgee99/Angular-Movie-App",
     },
@@ -26,7 +26,7 @@ export const projects = [
       title: "Meme Generator",
       subtitle: "React",
       description:
-        "This is a simple project built using React. Uses an API to generate a random meme image, and allows the user to add their own text.",
+        "This is a simple beginner project built using React. It uses a meme API to generate a random meme image, and allows the user to add their own text. ",
       image: project3Image,
       link: "https://github.com/georgee99/simple-meme-generator",
     },
@@ -34,7 +34,7 @@ export const projects = [
       title: "King Shark Chrome Extension",
       subtitle: "JavaScript",
       description:
-        "This is a simple Chrome Extension which changes all images on a webpage to images of the DC Character 'King Shark'.",
+        "This is a simple Chrome Extension which changes all images on a webpage to images of the DC Character 'King Shark'. It was built with only Vanilla JavaScript",
       image: project4Image,
       link: "https://github.com/georgee99/king-shark-chrome-extension",
     },
